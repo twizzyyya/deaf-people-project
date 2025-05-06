5979e72bbe2574b692173fe607a320db891a0cfe
twizzyyya <161097220+twizzyyya@users.noreply.github.com>
2025-05-06 12:05:31 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 input.onSound(DetectedSound.Loud, function () {
+    input.setSoundThreshold(SoundThreshold.Loud, 85)
     basic.showLeds(`
         . # . # .
         . # . # .
         . . . . .
         . # # # .
-        . # . # .
+        # . . . #
         `)
 })
 input.onSound(DetectedSound.Quiet, function () {
+    input.setSoundThreshold(SoundThreshold.Quiet, 70)
     basic.showLeds(`
         . # . # .
         . # . # .
